3c96a44443ae6ba7242422697cc7d2f2d24ea7d8
Anasmahfoud <156627676+Anasmahfoud@users.noreply.github.com>
2024-11-13 15:46:30 +0100

    init state & store.js file

---
+++ src/redux/reducers/produitReducers.js
@@ -0,0 +1,41 @@
+
+const initState= {
+    produits: [
+        {
+            clients : [
+                {
+                numero : '1',
+                nom: 'N1',
+                prenom : 'P1'
+                }
+            ] 
+            , 
+            produits : [
+                {
+                    codeProduit : '1' , 
+                    intitule : 'Produit1',
+                    prix : 10
+                },
+                {
+                    codeProduit : '2' , 
+                    intitule : 'Produit2',
+                    prix : 20
+                } , 
+                {
+                    codeProduit : '3' , 
+                    intitule : 'Produit3',
+                    prix : 30
+                }
+            ], 
+            achats : [
+                {
+                    numero : '1', 
+                    codeProduit : '1' ,
+                    qte : 5
+                }
+            ]
+        }
+    ]
+}
+
+
